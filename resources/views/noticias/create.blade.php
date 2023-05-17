d924749bb03f17da01f8eddc432187959e15cd33
David Ortiz <davidortiz25122010@gmail.com>
2023-05-17 19:26:10 -0400

    asdsad

--- resources/views/noticias/create.blade.php
+++ resources/views/noticias/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/ckeditor-styles.css') }}">
+@endsection
+
 @section('content')
     <div class="row mt-4">
         <div class="col-lg-10 col-md-10 col-sm-12">
@@ -53,7 +57,14 @@
                                 </div>
                             @endforeach
                         </div>
-                      <button type="submit" class="btn btn-succes mt-3">Crear</button>
+
+                        <div class="form-row mb-4">
+                            <div class="form-group col-md-12">
+                                <label for="inputEmail4">Contenido</label>
+                                <textarea name="contenido" id="contenido" cols="30" rows="10" class=""></textarea>
+                            </div>
+                        </div>
+                      <button type="submit" class="btn btn-success mt-3">Crear</button>
                     </form>
                 </div>
             </div>
@@ -62,3 +73,17 @@
 
 @endsection
 
+@section('js')
+    <script src="https://cdn.ckeditor.com/ckeditor5/37.1.0/classic/ckeditor.js"></script>
+    <script>
+       ClassicEditor
+        .create(document.querySelector('#contenido'), {
+            // Ruta del archivo CSS personalizado
+            contentsCss: '/css/ckeditor-styles.css'
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    </script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,7 +215,7 @@
     <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container">
             <div class="text-center">
-                <h6 class="section-title bg-white text-center text-primary px-3">Ultimas Noticias</h6>
+                <h6 class="section-title bg-white text-center text-primary px-3">Últimas Noticias</h6>
                 {{-- <h1 class="mb-5">Our Students Say!</h1> --}}
             </div>
             <div class="owl-carousel testimonial-carousel position-relative">
