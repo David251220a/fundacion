5a4e5c6bfe40d6be4456cd911822491b3dfff98b
Rodolfo Not <82409787+David251220a@users.noreply.github.com>
2023-08-08 13:39:28 -0400

    cambios
    cambios

--- app/Models/UnidadMedida.php
+++ app/Models/UnidadMedida.php
@@ -5,15 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TipoCurso extends Model
+class UnidadMedida extends Model
 {
     use HasFactory;
 
-    //para guardar todos los campos.
     protected $guarded = [];
 
-    public function usuario()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 }
